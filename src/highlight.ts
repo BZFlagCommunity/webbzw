4af8b08f4c5e5dbae1c6f8abed90eeedb90a2828
The Noah <thenoahbz@gmail.com>
2020-12-19 21:52:11 -0600

    Add performance benchmark to highlighting

--- src/highlight.ts
+++ src/highlight.ts
@@ -8,20 +8,35 @@ export const deleteHighlightElement = (editor: HTMLElement): void => {
 };
 
 export const highlight = (editor: HTMLElement): void => {
+  console.time("delete old element");
   deleteHighlightElement(editor);
+  console.timeEnd("delete old element");
 
   const textarea = editor.querySelector("textarea") as HTMLTextAreaElement;
 
+  console.time("create new element");
   const elem = document.createElement("pre");
   if(!elem){
     console.error("highlight element could not be created");
     return;
   }
   elem.classList.add("highlight");
+  console.timeEnd("create new element");
 
-  elem.innerHTML = highlightHtml(textarea.value);
+  console.time("build html");
+  const html = highlightHtml(textarea.value);
+  console.timeEnd("build html");
 
+  console.time("set html");
+  elem.innerHTML = html;
+  console.timeEnd("set html");
+
+  console.time("add element to dom");
   editor.appendChild(elem);
+  console.timeEnd("add element to dom");
+
+  console.time("set element scroll");
   elem.scrollTop = textarea.scrollTop;
   elem.scrollLeft = textarea.scrollLeft;
+  console.timeEnd("set element scroll");
 };
