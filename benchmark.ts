94d65cde8bf53e9e3c9d02d1c03acd4ae0e467fd
The Noah <thenoahbz@gmail.com>
2021-01-22 17:30:24 -0600

    Add benchmark

--- benchmark.ts
+++ benchmark.ts
@@ -1,53 +1,19 @@
+import {bench, runBenchmarks} from "https://deno.land/std@0.84.0/testing/bench.ts";
+
 import {highlightHtml} from "./src/highlight/core.ts";
 
-const COUNT = 10000;
+const source = await fetch("https://raw.githubusercontent.com/The-Noah/tank-wars/master/map.bzw").then((res) => res.text());
 
-const start = performance.now();
+bench({
+  name: "hightlight sample world",
+  runs: 1000,
+  func(b): void{
+    b.start();
 
-for(let i = 0; i < COUNT; i++){
-  highlightHtml(`# sample world
+    highlightHtml(source);
 
-world
-  size 200
-end
+    b.stop();
+  },
+});
 
-box
-  position 0 0 0
-  size 30 30 15
-  rotation 45
-end
-
-pyramid
-  position 50 50 0
-  size 5 5 50
-end
-
-pyramid
-  position -50 50 0
-  size 5 5 50
-end
-
-pyramid
-  position 50 -50 0
-  size 5 5 50
-end
-
-pyramid
-  position -50 -50 0
-  size 5 5 50
-end
-
-base
-  position -170 0 0
-  size 30 30 .5
-  color 1
-end
-
-base
-  position 170 0 0
-  size 30 30 .5
-  color 2
-end`);
-}
-
-console.log(`${COUNT} highlight html in ${performance.now() - start}ms`);
+runBenchmarks();
