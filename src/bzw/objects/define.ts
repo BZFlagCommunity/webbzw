bedeb3fb4045de29aa1d9859f9acec6f348e6636
The Noah <thenoahbz@gmail.com>
2021-01-29 14:49:24 -0600

    Remove unused `current` from `define`

--- src/bzw/objects/define.ts
+++ src/bzw/objects/define.ts
@@ -13,8 +13,6 @@ export class Define extends MapObject{
   id: string = "";
   children: MapObject[] = [];
 
-  private current: string = "";
-
   constructor(line?: string){
     super(line);
 
@@ -39,22 +37,15 @@ export class Define extends MapObject{
   parseLine(line: string): void{
     // we don't call super becase we don't use any of the normal properties
 
-    if(line === "end"){
-      this.current = "";
-    }else if(line === "box"){
-      this.current = line;
+    if(line === "box"){
       this.children.push(new Box(line));
     }else if(line === "meshbox"){
-      this.current = line;
       this.children.push(new MeshBox(line));
     }else if(line === "pyramid"){
-      this.current = line;
       this.children.push(new Pyramid(line));
     }else if(line === "meshpyr"){
-      this.current = line;
       this.children.push(new MeshPyramid(line));
     }else if(line === "base"){
-      this.current = line;
       this.children.push(new Base(line));
     }else if(this.children.length > 0){
       this.children[this.children.length - 1].parseLine(line);
