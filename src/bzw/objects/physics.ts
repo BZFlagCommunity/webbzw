1bad820e64df1c80f0efd5d6c44f6297d81e7216
The Noah <thenoahbz@gmail.com>
2021-04-19 13:25:53 -0500

    Add linear property to physics

--- src/bzw/objects/physics.ts
+++ src/bzw/objects/physics.ts
@@ -5,6 +5,7 @@ export class Physics extends MapObject{
   HEADER = "physics";
 
   slide: number = 0;
+  linear: [number, number, number] = [0, 0, 0];
 
   buildMesh(): void{
   }
@@ -16,6 +17,8 @@ export class Physics extends MapObject{
 
     if(parts[0] === "slide"){
       this.slide = parseNum(parts[1]) || 0;
+    }else if(parts[0] === "linear"){
+      this.linear = [parseNum(parts[1], 0), parseNum(parts[2], 0), parseNum(parts[3], 0)];
     }
   }
 }
